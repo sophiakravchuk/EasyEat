20de0d8e70bfe37d78a3802b2c48060ddf2ebe78
Sony Haletska <shaletska@gmail.com>
2021-06-28 22:47:33 +0300

    recipe screen update

--- lib/screens/recipe_screen.dart
+++ lib/screens/recipe_screen.dart
@@ -3,21 +3,32 @@ import 'dart:convert';
 import 'package:EasyEat/screens/constants.dart';
 import 'package:EasyEat/screens/shared_preferences.dart';
 import 'package:EasyEat/screens/top_bar.dart';
+import 'package:EasyEat/services/api_services.dart';
+import 'package:EasyEat/services/meal_model.dart';
+import 'package:EasyEat/services/recipe_parser.dart';
 import 'package:flutter/material.dart';
 
 class RecipePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _RecipePageState();
-  RecipePage();
+  Meal meal;
+  RecipeInstructions instructions;
+  RecipePage(this.meal, this.instructions);
 }
 
 class _RecipePageState extends State<RecipePage> {
 
+  LocalStorage storage;
+
   @override
   void initState() {
     super.initState();
+    initial();
   }
 
+  void initial() async {
+    storage = LocalStorage();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,76 +36,147 @@ class _RecipePageState extends State<RecipePage> {
       appBar: TopBar().build(context),
       body: ListView(
           children: <Widget>[
-            Container(
-              child: Card(
-                // shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                child: InkWell(
-                  // onTap: () => print("ciao"),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,  // add this
-                    children: <Widget>[
-                      ClipRRect(
-                        child: Image.network(
-                            'https://spoonacular.com/recipeImages/660261-556x370.jpg',
-                            // width: 300,
-                            height: 150,
-                            fit:BoxFit.fill
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+            _topImage(context),
+            SizedBox(
+              height: 20,
             ),
-          _topText(context)
+            _topText(context),
+            SizedBox(
+              height: 20,
+            ),
+            _instructionsText(context),
+            SizedBox(
+              height: 20,
+            ),
+            _recipeText(context),
+            SizedBox(
+              height: 30,
+            ),
           ]
       ),
     );
   }
 
+  Widget _likeButton() =>
+      IconButton(
+        icon: Icon(Icons.favorite),
+        color: widget.meal.isFavourite
+            ? EasyEatColors.orange
+            : EasyEatColors.grey,
+        onPressed: () {
+          setState(() {
+            widget.meal.isFavourite = !widget.meal.isFavourite;
+            if (widget.meal.isFavourite) {
+              storage.setRecipe(widget.meal.id.toString(),
+                  jsonEncode(widget.meal.toJson()));
+            } else {
+              storage.removeRecipe(widget.meal.id.toString());
+            }
+          });
+        },
+      );
+
+
   Widget _topText(BuildContext context) =>
       Row(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.fromLTRB(4, 2, 18, 18),
-              child: Container(
-                child: Text(
-                "Name of the meal",
+            padding: EdgeInsets.fromLTRB(30, 0, 18, 0),
+            child: Container(
+              width: 330,
+              child: Text(
+                widget.meal.title,
                 style: EasyEatTextStyle(
-                fontSize: 24,
-                textColor: EasyEatColors.black,
+                  fontSize: 28,
+                  textColor: EasyEatColors.darkGreen,
                 ).style(),
-                ),
+              ),
             ),
           ),
         ],
       );
 
 
-  Widget _topImage(BuildContext context) =>
-      Row(
-        children: <Widget>[
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,  // add this
-              children: <Widget>[
-                ClipRRect(
-                  child: Image.network(
-                      "https://spoonacular.com/recipeImages/660261-556x370.jpg",
-                      // width: 300,
-                      height: 150,
-                      fit:BoxFit.fill
-
+  Widget _instructionsText(BuildContext context) {
+    List<Row> list = new List<Row>();
+    for (var i = 0; i < widget.instructions.ings.length; i++) {
+      String ingredient = widget.instructions.ings[i].amount.toString() + " ";
+      if (widget.instructions.ings[i].units != null) {
+        ingredient += widget.instructions.ings[i].units  + " ";
+      }
+      ingredient += widget.instructions.ings[i].name;
+      list.add(Row(
+          children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Text(
+                      ingredient,
+                      style: EasyEatTextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                        textColor: EasyEatColors.green,
+                      ).style(),
+                    ),
                   ),
-                ),
-                ListTile(
-                  title: Text('Pub 1'),
-                  subtitle: Text('Location 1'),
-                ),
-              ],
+            SizedBox(
+              height: 10,
+            ),
+          ]
+      ),
+      );
+    }
+    return new Column(children: list);
+  }
+
+
+  Widget _recipeText(BuildContext context) {
+    List<Row> list = new List<Row>();
+    for (var i = 0; i < widget.instructions.steps.length; i++) {
+      list.add(Row(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(30, 1, 30, 1),
+            child: Container(
+              width: 330,
+              child: Text(
+                widget.instructions.steps[i],
+                style: EasyEatTextStyle(
+                  fontSize: 19,
+                  textColor: EasyEatColors.darkGreen,
+                  fontWeight: FontWeight.normal,
+                ).style(),
+              ),
             ),
           ),
-          ]
+        ],
+      ),
       );
+    }
+    return new Column(children: list);
+  }
+
+  Widget _topImage(BuildContext context) =>
+      Container(
+        height: 150,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: Image.network(
+                  widget.meal.imgURL,
+                  height: 150,
+                  fit: BoxFit.fill
+              ).image,
+              fit: BoxFit.cover),
+        ),
+        child:  Container(
+          alignment: Alignment(0.9, -0.8),
+          child: CircleAvatar(
+            radius: 25,
+            backgroundColor: EasyEatColors.white,
+            child: _likeButton(),
+          ),
+      ),
+      );
+
+
 
 }
